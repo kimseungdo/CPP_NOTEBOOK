e74175f38d641cc8658f99edb641f0fa31af018d
Ben Steiger <rkrkakak456@gmail.com>
2019-11-17 00:06:59 +0900

    Add Local file

---
+++ CPP/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int c=0; int d=100;
+
+int main(void){
+    int a=2; int b=2; int c=a+b;
+
+    std::cout<<a<<"+"<<b<<"="<<c<<std::endl;
+    return 0;
+}
---
+++ Opencv_CPP/CMakeFiles/Opencv_CPP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Opencv_CPP.dir/main.cpp.obj"
+  "Opencv_CPP.exe"
+  "Opencv_CPP.exe.manifest"
+  "Opencv_CPP.pdb"
+  "libOpencv_CPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Opencv_CPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
